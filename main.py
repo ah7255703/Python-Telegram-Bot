da8f505243df9890135324c0a2ab95a6db6b56db
Ahmed Hassan <ah7255703@gmail.com>
2021-07-09 23:09:27 +0200

    add main files

---
+++ main.py
@@ -0,0 +1,71 @@
+import video_D , validators
+import os 
+from os import environ
+from telegram.ext import *
+API_KEY = environ['API_KEY']
+print("Bot Started....")
+def userinfo(update,context):
+    messager_id = update.message.chat.id
+    messager_Fname = update.message.chat.first_name
+    messager_Lname = update.message.chat.last_name
+    messager_Username = update.message.chat.username
+    info = """
+          _______________________________________
+          id = {}
+          First Name = {}
+          Last Name = {}
+          UserName = {}
+          _______________________________________
+          """.format(messager_id,messager_Fname,messager_Lname,messager_Username)
+    return info
+#<start> commands Functions 
+def start_command(update,context):
+    update.message.reply_text(' Hello {}\n, Send Facebook Video Link to start Downloading !'.format(update.message.chat.first_name))
+def help_command(update,context):
+    update.message.reply_text('This Bot can Bring you Download Link for facebook Video \n ,if url was given ')
+def suggestions_command(update,context):
+    userinfo(update,context)
+#<End> command functions 
+
+#<start> error function 
+def error_Functiuon(update,context):
+    print("update {} caused error {}".format(update,context.error))
+#<End> error function 
+
+
+
+
+
+
+
+#<start> message handler
+def message_handler(update,context):
+    message_text = update.message.text
+    MSG = message_text.strip()
+    print('The message: {}'.format(MSG),userinfo(update,context))
+    if validators.url(MSG) == False : 
+        update.message.reply_text('{} Not Valid Url , Try Valid one '.format(message_text))
+    else:
+        Dict = video_D.Video_D(MSG)
+        update.message.reply_text('The requested Video is : {} \n and the Link : {}'.format(Dict['Title'],Dict['D_Links'][0])),
+        update.message.reply_text('and the audio format  : {}'.format(Dict['D_Links'][1]))
+        
+ 
+ 
+ 
+ 
+ 
+ 
+            
+#<End> message 
+def main():
+    Bot_updater = Updater(API_KEY,use_context=True)
+    Bot_dispatcher = Bot_updater.dispatcher
+    Bot_dispatcher.add_handler(CommandHandler('start',start_command))
+    Bot_dispatcher.add_handler(CommandHandler('help',help_command))
+    Bot_dispatcher.add_handler(CommandHandler('userinfo',suggestions_command))
+    Bot_dispatcher.add_handler(MessageHandler(Filters.text,message_handler))
+    Bot_dispatcher.add_error_handler(error_Functiuon)
+    Bot_updater.start_polling(10)
+    Bot_updater.idle()
+main()
---
+++ video_D.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import validators
+
+
+def Video_D(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Origin': 'https://www.getfvid.com','Alt-Used': 'www.getfvid.com','Connection': 'keep-alive',
+        'Referer': 'https://www.getfvid.com/','Upgrade-Insecure-Requests': '1','Pragma': 'no-cache','Cache-Control': 'no-cache','TE': 'Trailers',}
+
+    # Validating The Video Url 
+    valid = validators.url(url)
+    if valid == True :
+        isValid = True
+    else:
+        isValid = False
+    # requesting the server to get markup response
+    response = requests.post('https://www.getfvid.com/downloader', headers=headers,data={'url': url}).content
+    soup = BeautifulSoup(response,'html.parser')
+    # if the video Private or not 
+    alert = soup.select_one('.alert')
+    if alert == "None":
+        ISprivate = True
+    else:
+        ISprivate = False
+    # isavailable Video 
+    if ISprivate == True or isValid == False :
+        ISavailable = False
+    else:
+        ISavailable = True
+    try:
+        V_title = soup.find('input',{'type':'hidden'}).get('value')
+        D_video_link = soup.select_one('.btns-download > p:nth-child(2) > a:nth-child(1)').get('href')
+        D_audio_link = soup.select_one('.btns-download > p:nth-child(3) > a:nth-child(1)').get('href')
+    except:
+        V_title = D_audio_link = D_video_link = "Not Available"
+    Video_Data = {'FbLink':url,'Private':ISprivate ,'Title':V_title,'Valid':isValid,'Available':ISavailable,'D_Links':[D_video_link,D_audio_link]}
+    return Video_Data
